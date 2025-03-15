c3414cf9e20402606b7f7d8d3856f2cbb3a66595
Sylvio Ximenez de Azevedo Neto <sylvioazevedo@gmail.com>
2025-03-15 01:56:15 -0300

    organisation

---
+++ yf_option_data.py
@@ -0,0 +1,39 @@
+import datetime
+import pandas as pd
+import numpy as np
+import yfinance as yf
+
+def options_chain(symbol):
+    tk = yf.Ticker(symbol)
+    # Expiration dates
+    exps = tk.options
+
+    # Get options for each expiration
+    options = pd.DataFrame()
+    for e in exps:
+        opt = tk.option_chain(e)
+        opt = pd.concat([opt.calls, opt.puts], ignore_index=True)
+        opt['expirationDate'] = e
+        options = pd.concat([options, opt], ignore_index=True)
+
+    # Bizarre error in yfinance that gives the wrong expiration date
+    # Add 1 day to get the correct expiration date
+    options['expirationDate'] = pd.to_datetime(options['expirationDate']) + datetime.timedelta(days = 1)
+    options['dte'] = (options['expirationDate'] - datetime.datetime.today()).dt.days / 365
+    
+    # Boolean column if the option is a CALL
+    options['CALL'] = options['contractSymbol'].str[4:].apply(
+        lambda x: "C" in x)
+    
+    options[['bid', 'ask', 'strike']] = options[['bid', 'ask', 'strike']].apply(pd.to_numeric)
+    options['mark'] = (options['bid'] + options['ask']) / 2 # Calculate the midpoint of the bid-ask
+    
+    # Drop unnecessary and meaningless columns
+    options = options.drop(columns = ['contractSize', 'currency', 'change', 'percentChange', 'lastTradeDate', 'lastPrice'])
+
+    return options
+
+if __name__ == '__main__':
+    symbol = 'AAPL'
+    options = options_chain(symbol)
+    print(options.head())
